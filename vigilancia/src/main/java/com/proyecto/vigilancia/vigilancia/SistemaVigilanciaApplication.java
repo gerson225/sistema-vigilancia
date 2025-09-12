666e26d4f42fbd43582f74a216293c8d019d9b88
Usuario Git <famaches@gmail.com>
2025-09-11 20:25:22 -0500

    Avance: agregado README con objetivos

---
+++ vigilancia/src/main/java/com/proyecto/vigilancia/vigilancia/SistemaVigilanciaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.vigilancia.vigilancia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVigilanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVigilanciaApplication.class, args);
+	}
+
+}
---
+++ vigilancia/src/test/java/com/proyecto/vigilancia/vigilancia/SistemaVigilanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.vigilancia.vigilancia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVigilanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
